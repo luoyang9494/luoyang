bdddbb3396c1ba15ba1763b750d92ee7917e9bbd
luoyang <luoyang13579@126.com>
2018-10-09 09:14:26 +0800

    完成

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,31 @@
-// $.ajax({
-//     url: "./data/data.json",
-//     success: function(data) {
-//         console.log(data);
-//     }
-// })
-
-$(".section").on("click", "div", function() {
-    $(this).css("border", "2px solid #f4a59f").siblings().css("border", "1px solid #ccc");
-    $(this).find(".ipt").css("checked", "checked");
+$.ajax({
+    url: "data/data.json",
+    success: function(data) {
+        rander(data);
+    }
 })
+
+function rander(data) {
+    var html = '';
+    $.each(data, function(k, v) {
+        html += `<div>
+                    <p class="top">
+                        <span><input type="radio" class="ipt">${v.name}</span>
+                        <span>${v.day}</span>
+                    </p>
+                    <ul class="bottom">
+                        <li>${v.cpu}<span>CPU</span></li>
+                        <li>${v.storage}<span>内存</span></li>
+                        <li>${v.system}<span>系统盘</span></li>
+                        <li>${v.wsx}<span>数据盘</span></li>
+                    </ul>
+                </div>`
+    });
+
+    $('.section').append(html);
+    $(".section").on("click", "div", function() {
+        $(this).css("border", "2px solid #f4a59f").siblings().css("border", "1px solid #ccc");
+        $(this).find('input[class="ipt"]').prop("checked", true);
+        $(this).siblings().find('input[class="ipt"]').prop("checked", false);
+    })
+}
